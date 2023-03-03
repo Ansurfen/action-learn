1e5de1480af639d2cc829be6599063a027665c6e
ansurfen <axf593161@gmail.com>
2023-03-04 00:17:09 +0800

    init

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
+
 with open('./test.txt', 'w+', encoding='utf-8') as fp:
     fp.write('aaa')
     fp.close()
 with open('./home.txt', 'w+', encoding='utf-8') as fp:
     fp.write('aaa')
     fp.close()
-print("ok")
+print(os.getcwd())
