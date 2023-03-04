970528aac7e368a206a971d9acb4da7a9c45f1d3
ansurfen <axf593161@gmail.com>
2023-03-04 09:59:43 +0800

    init

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
+# from bs4 import BeautifulSoup
+import requests
 
-with open('./test.txt', 'w+', encoding='utf-8') as fp:
-    fp.write('aaa')
+url = 'https://central.sonatype.com/v1/browse/component_versions'
+payload = dict(sortField="normalizedVersion", sortDirection="desc",
+               page=0, size=10, filter=["name:spring"])
+headers = {"content-type": "application/json"}
+req = requests.post(url, json=payload, headers=headers)
+with open('test.json', 'w+') as fp:
+    fp.write(req.text)
     fp.close()
-with open('./home.txt', 'w+', encoding='utf-8') as fp:
-    fp.write('aaa')
-    fp.close()
-print(os.getcwd())
