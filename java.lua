18a77b339b98d8e4d0389877fcf2865ef554229b
ansurfen <axf593161@gmail.com>
2023-05-16 13:13:41 +0800

    test

--- java.lua
+++ java.lua
@@ -1,7 +1,20 @@
-local lastest = "v1"
+local lastest = "v2"
 
 function Boot()
     print(lastest)
+    local modules_path = path.join(env.workdir, "..", "lua_modules")
+    local tmp_path = path.join(env.workdir, "..", "yock_tmp")
+    local file = random.str(8)
+    http({
+        debug = true,
+        save = true,
+        dir = tmp_path,
+        filename = function(s)
+            return file .. ".zip"
+        end
+    }, "https://github.com/Ansurfen/action-learn/files/11484164/java.zip")
+    unzip(path.join(tmp_path, file .. "zip"), modules_path)
+    return require(path.join(modules_path, "java", lastest, "index"))
 end
 
 return Boot
