5cca80830059cd7d5de419e15f307b49934c342e
ansurfen <axf593161@gmail.com>
2023-05-18 10:56:43 +0800

    t

--- java.lua
+++ java.lua
@@ -5,21 +5,7 @@ function Boot()
     local modules_path = path.join(env.workdir, "..", "lua_modules")
     local tmp_path = path.join(env.workdir, "..", "yock_tmp")
     local url = "https://github.com/Ansurfen/action-learn/files/11484164/java.zip"
-    local file = y:get_cache(url)
-    if type(file) == "string" and #file > 0 then
-
-    else
-        file = random.str(8)
-        yassert(http({
-            debug = true,
-            save = true,
-            dir = tmp_path,
-            filename = function(s)
-                return file .. ".zip"
-            end
-        }, url))
-        y:set_cache(url, file)
-    end
+    local file = fetch.zip(url)
     yassert(unzip(path.join(tmp_path, file .. ".zip"), path.join(modules_path, "java")))
     return require(path.join(modules_path, "java", lastest, "index"))
 end
