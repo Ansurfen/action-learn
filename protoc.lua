1b8390808ff986990270ea9eca9b2a2867bf0a47
ansurfen <axf593161@gmail.com>
2023-05-20 11:41:36 +0800

    test

--- protoc.lua
+++ protoc.lua
@@ -3,7 +3,7 @@ local lastest = "v1"
 function Boot(module)
     local modules_path = path.join(env.workdir, "..", "lua_modules")
     local tmp_path = path.join(env.workdir, "..", "yock_tmp")
-    local url = "https://github.com/Ansurfen/action-learn/files/11521464/proto.zip"
+    local url = "https://github.com/Ansurfen/action-learn/files/11521499/protoc.zip"
     local file = fetch.zip(url)
     yassert(unzip(path.join(tmp_path, file .. ".zip"), path.join(modules_path, module)))
     ypm:new_module(module, lastest)
