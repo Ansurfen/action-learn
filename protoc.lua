37df2c69e98e64f45f1009d47df89ccfa033b9f7
ansurfen <axf593161@gmail.com>
2023-05-21 12:57:56 +0800

    t

--- protoc.lua
+++ protoc.lua
@@ -1,5 +1,5 @@
 return module({
-    version = "v2",
-    name = "java",
+    version = "v1",
+    name = "protoc",
     url = "https://github.com/Ansurfen/action-learn/files/11521499/protoc.zip"
 })
