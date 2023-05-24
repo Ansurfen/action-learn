cee3bde2356dddd9c5f3e634eb982eadc062cacf
ansurfen <axf593161@gmail.com>
2023-05-25 00:01:00 +0800

    aaa

--- main.py
+++ main.py
@@ -7,18 +7,9 @@ import subprocess
 
 
 def zip(source_dir: str, target_file: str):
-    """
-    Compresses the contents of a directory using the zip command.
-
-    :param source_dir: The directory to compress.
-    :param target_file: The name of the archive file to create.
-    """
-    # Create the zip command with the appropriate arguments
     cmd = ['zip', '-r', '-X', target_file, source_dir]
-
-    # Call the command using subprocess.run()
     try:
-        result = subprocess.run(cmd, check=True)
+        subprocess.run(cmd, check=True)
     except subprocess.CalledProcessError as e:
         raise Exception(
             f"Failed to compress directory {source_dir}. Error: {e}")
