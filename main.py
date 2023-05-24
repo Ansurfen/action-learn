b158ad541a21fb0605f721250b36b1e1e6217603
ansurfen <axf593161@gmail.com>
2023-05-24 15:55:31 +0800

    t

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 import os
 import subprocess
 import shutil
+import time
+timestamp = time.time()
 
-# 获取需要打包的目录路径
+
+def zip_dir(source_dir: str, target_file: str):
+    """
+    Compresses the contents of a directory using the zip command.
+
+    :param source_dir: The directory to compress.
+    :param target_file: The name of the archive file to create.
+    """
+    # Create the zip command with the appropriate arguments
+    cmd = ['zip', '-r', target_file, source_dir]
+
+    # Call the command using subprocess.run()
+    try:
+        result = subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError as e:
+        raise Exception(
+            f"Failed to compress directory {source_dir}. Error: {e}")
+
+    print(f"Successfully compressed directory {source_dir} into {target_file}")
+
+
 directory = os.environ.get('INPUT_DIRECTORY')
 if directory is None:
     raise ValueError('Missing required input parameter: directory')
 
-# 获取 release 版本号
-version = os.environ.get('INPUT_VERSION')
+version = int(timestamp)
 if version is None:
     raise ValueError('Missing required input parameter: version')
 
-# 创建临时目录
 temp_dir = 'temp'
 os.makedirs(temp_dir)
 
-# 复制需要打包的文件到临时目录中
 for item in os.listdir(directory):
     item_path = os.path.join(directory, item)
     if os.path.isfile(item_path):
         shutil.copy2(item_path, temp_dir)
 
-# 打包临时目录
 subprocess.run(['tar', '-czf', f'{version}.tar.gz', temp_dir])
-
-# 发布 Release
-subprocess.run(['gh', 'release', 'create', f'v{version}', f'{version}.tar.gz'], check=True)
+zip_dir("/opencmd", f"{version}.zip")
+subprocess.run(['gh', 'release', 'create',
+               f'v{version}', f'{version}.tar.gz', f'{version}.zip'], check=True)
